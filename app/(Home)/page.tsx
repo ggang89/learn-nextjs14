5657cf6660e3e8f4513e30b71d4400d416b8916e
ggang89 <ggang89@gmail.com>
2025-03-20 22:56:18 +0900

    client에서 필요한 data fetch 하기

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,14 +1,37 @@
+'use client'
 
-export const metadata = {
-  title:"Home"
-}
+import { useEffect, useState } from "react";
+
+
 
 export default function Page() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    ); 
+    const data = await response.json();
+    setMovies(data);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
+  
 
   return (
     <div>
-
       <h1>Home!</h1>
+      {loading ? (
+        <h2>lodaing....</h2>
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
